08d85184d0c72179c8f3a0d81b036da6657b0089
joaquinegl <joaquin.egl@gmail.com>
2023-07-09 13:40:57 +0200

    +datos personales, salario promedio, obj persona.

--- simulador.js
+++ simulador.js
@@ -1,28 +1,78 @@
-function calcularHipoteca(valorSalario) {
+function calcularHipoteca(salarioPromedio) {
 
-    if (valorSalario > 10000) {
-        return valorSalario * 0
+    if (salarioPromedio > 10000) {
+        return salarioPromedio * 0
     }
-
-    if (valorSalario > 1000) {
-        return valorSalario * 150
+    
+    if (salarioPromedio > 1000) {
+        return salarioPromedio * 150
     }  
-    return valorSalario * 200
+
+    if (salarioPromedio < 1000)
+        return salarioPromedio * 200
 }
 
-let salario
-do {
-    salario = prompt("Ingrese su salario bruto mensual")
+let nombre = prompt("Ingrese su nombre completo")
+let edad = prompt("Ingrese su edad")
+let ciudad = prompt("Ingrese su ciudad actual")
 
-    if (isNaN(salario)) {
+let ultimoSalario
+do {
+    ultimoSalario = parseInt(prompt("Ingrese su salario bruto mensual"))
+
+    if (isNaN(ultimoSalario)) {
         alert("Ingrese un valor numérico")
     }
 
-    if (salario <= 0) {
+    if (ultimoSalario <= 0) {
         alert("Ingrese un número mayor a 0")
     }
-} while (salario <= 0 || isNaN(salario))
+} while (ultimoSalario <= 0 || isNaN(ultimoSalario))
 
-let resultadoHipoteca = calcularHipoteca(salario)
 
-alert("Nuestro banco puede ofrecerte " + resultadoHipoteca + " patacones")
+
+let anteUltimoSalario
+do {
+    anteUltimoSalario = parseInt(prompt("Ingrese el salario bruto mensual de su trabajo anterior"))
+
+    if (isNaN(anteUltimoSalario)) {
+        alert("Ingrese un valor numérico")
+    }
+
+    if (anteUltimoSalario <= 0) {
+        alert("Ingrese un número mayor a 0")
+    }
+} while (anteUltimoSalario <= 0 || isNaN(anteUltimoSalario))
+
+
+
+const listaDeSalarios = []
+
+listaDeSalarios.push(ultimoSalario, anteUltimoSalario)
+console.log(listaDeSalarios)
+
+
+function calcularPromedio(valor1, valor2){
+    return(valor1 + valor2)/2
+} 
+
+
+let salarioPromedio = calcularPromedio(ultimoSalario, anteUltimoSalario)
+
+let resultadoHipoteca = calcularHipoteca(salarioPromedio)
+
+alert("Nuestro banco puede ofrecerte " + resultadoHipoteca + " patacones" + " Sr/a " + nombre)
+
+
+let persona = {
+    nombre: nombre,
+    edad: edad, 
+    ciudad: ciudad, 
+    salarioActual: ultimoSalario, 
+    salarioAnterior: anteUltimoSalario,
+    salarioPromedio: salarioPromedio, 
+    prestamoPosible: resultadoHipoteca
+}
+
+console.table(persona)
+
